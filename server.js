fa504d267ffff4080c666dcc4231ef5b02184061
kerdlix <danielbgg@gmail.com>
2017-12-07 11:13:44 -0200

    Lecture 44

--- server.js
+++ server.js
@@ -1,22 +1,31 @@
 const express = require('express');
+const hbs = require('hbs');
 
 var app = express();
 
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
 
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+});
+
 app.get('/', (req, res) => {
-  //res.send('<h1>Hello Express!</h1>');
-  res.send({
-    name: 'Daniel',
-    likes: [
-      'computer',
-      'cities'
-    ]
+  res.render('home.hbs', {
+    pageTitle: 'Home Page',
+    welcomeMessage: 'Welcome to this new site!!!'
   });
 });
 
 app.get('/about', (req, res) => {
-  res.send('About Page');
+  res.render('about.hbs', {
+    pageTitle: 'About Page'
+  });
 });
 
 app.get('/bad', (req, res) => {
